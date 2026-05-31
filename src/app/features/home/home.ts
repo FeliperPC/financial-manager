177c276bd8573651984dd52409018a3e95fc6380
Felipe Padovani <felipepadovani@rubeus.com.br>
2026-05-31 14:27:15 -0300

    Enhance home component to display transaction items in the template
    - Updated home component to include a loop for rendering transaction items using the new transaction item component.
    - Modified transactions signal to include titles for better clarity and organization.
    - Imported the transaction item component to the home component for improved modularity.

--- src/app/features/home/home.ts
+++ src/app/features/home/home.ts
@@ -1,16 +1,19 @@
 import { Component, signal } from '@angular/core';
 import { Balance } from './components/balance/balance';
+import { TransactionItem } from './components/transaction-item/transaction-item';
+import { TransactionType } from '../../shared/transactions/interfaces/enums/transaction-type';
+import { Transaction } from '../../shared/transactions/interfaces/transaction';
 
 @Component({
   selector: 'app-home',
-  imports: [Balance],
+  imports: [Balance, TransactionItem],
   templateUrl: './home.html',
   styleUrl: './home.scss',
 })
 export class Home {
-  transactions = signal([
-    { value: 200, type: 'income' },
-    { value: 100, type: 'outcome' },
-    { value: 100, type: 'outcome' },
+  transactions = signal<Transaction[]>([
+    { title: 'Salary', value: 200, type: TransactionType.INCOME },
+    { title: 'Shopping', value: 100, type: TransactionType.OUTCOME },
+    { title: 'Pet shop', value: 100, type: TransactionType.OUTCOME },
   ]);
 }
