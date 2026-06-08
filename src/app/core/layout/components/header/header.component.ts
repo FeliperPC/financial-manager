54d549c9cdd321c8da0a1709f210ceadca8d6a89
Felipe Padovani <felipepadovani@rubeus.com.br>
2026-06-08 16:30:25 -0300

    refact : moved the logout logic to a directive

--- src/app/core/layout/components/header/header.component.ts
+++ src/app/core/layout/components/header/header.component.ts
@@ -1,9 +1,6 @@
-import { Component, computed, inject } from '@angular/core';
+import { Component } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatToolbarModule } from '@angular/material/toolbar';
-import { LogoutFacade } from '../../../auth/facades/logout-facade';
-import { LoggedUser } from '../../../auth/stores/logged-user';
-import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-header',
@@ -11,16 +8,4 @@ import { Router } from '@angular/router';
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss',
 })
-export class Header {
-  private readonly logoutFacade = inject(LogoutFacade);
-  private readonly store = inject(LoggedUser);
-  private readonly router = inject(Router);
-
-  isLoggedIn = computed(() => this.store.isLoggedIn());
-
-  logout() {
-    this.logoutFacade.logout().subscribe({
-      next: () => this.router.navigate(['auth/login']),
-    });
-  }
-}
+export class Header {}
