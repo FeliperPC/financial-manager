d7c1f1a5fd720e61d1c511daacd4fec2559361d1
Felipe Padovani <felipepadovani@rubeus.com.br>
2026-06-08 16:30:46 -0300

    feat: implement sidenav for layout component
    - Replaced the existing layout structure with a Material sidenav for improved navigation.
    - Added SidenavItemsComponent to manage navigation links and logout functionality.
    - Introduced styles for the sidenav container and content.

---
+++ src/app/core/layout/components/sidenav-items/sidenav-items.component.ts
@@ -0,0 +1,29 @@
+import { Component, computed, inject, signal } from '@angular/core';
+import { MatListModule } from '@angular/material/list';
+import { RouterLink, RouterLinkActive } from '@angular/router';
+import { MatButtonModule } from '@angular/material/button';
+import { LogoutDirectivesDirective } from './directives/logout.directive';
+import { LoggedUser } from '../../../auth/stores/logged-user';
+
+@Component({
+  selector: 'app-sidenav-items',
+  imports: [
+    MatListModule,
+    RouterLink,
+    RouterLinkActive,
+    MatButtonModule,
+    LogoutDirectivesDirective,
+  ],
+  templateUrl: './sidenav-items.component.html',
+  styleUrl: './sidenav-items.component.scss',
+})
+export class SidenavItemsComponent {
+  private readonly store = inject(LoggedUser);
+  isLoggedIn = computed(() => this.store.isLoggedIn());
+  links = signal([
+    {
+      label: 'Home',
+      url: '/',
+    },
+  ]);
+}
--- src/app/core/layout/layout.component.ts
+++ src/app/core/layout/layout.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { Header } from './components/header/header.component';
 import { RouterOutlet } from '@angular/router';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { SidenavItemsComponent } from './components/sidenav-items/sidenav-items.component';
 
 @Component({
   selector: 'app-layout',
-  imports: [Header, RouterOutlet],
+  imports: [Header, RouterOutlet, MatSidenavModule, SidenavItemsComponent],
   templateUrl: './layout.component.html',
   styleUrl: './layout.component.scss',
 })
