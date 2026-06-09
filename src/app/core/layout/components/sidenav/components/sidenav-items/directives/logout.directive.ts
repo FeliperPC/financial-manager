72486f553c36a2b653d287da183e909b021feda6
Felipe Padovani <felipepadovani@rubeus.com.br>
2026-06-09 09:38:06 -0300

    feat: add LogoutDirective for user logout functionality
    - Introduced LogoutDirective to handle user logout on click events.
    - Integrated LogoutFacade for logout logic and navigation to the login page upon successful logout.

---
+++ src/app/core/layout/components/sidenav/components/sidenav-items/directives/logout.directive.ts
@@ -0,0 +1,20 @@
+import { Directive, inject } from '@angular/core';
+import { Router } from '@angular/router';
+import { LogoutFacade } from '@auth/facades/logout-facade';
+
+@Directive({
+  selector: '[appLogout]',
+  host: {
+    '(click)': 'logout()',
+  },
+})
+export class LogoutDirectivesDirective {
+  private readonly logoutFacade = inject(LogoutFacade);
+  private readonly router = inject(Router);
+
+  logout() {
+    this.logoutFacade.logout().subscribe({
+      next: () => this.router.navigate(['auth/login']),
+    });
+  }
+}
