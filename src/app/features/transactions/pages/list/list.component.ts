350719c48ca79797b5ba37e95b4972fd4891bf8e
Felipe Padovani <felipepadovani@rubeus.com.br>
2026-06-10 16:21:03 -0300

    reafact : improved with rxResource no need to debaunce

--- src/app/features/transactions/pages/list/list.component.ts
+++ src/app/features/transactions/pages/list/list.component.ts
@@ -1,4 +1,4 @@
-import { Component, inject, resource, signal } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
 import { ActivatedRoute, Router, RouterLink } from '@angular/router';
 import { Confirmation } from '@shared/dialog/confirmation/services/confirmation.service';
 import { Feedback } from '@shared/feedback/services/feedback.service';
@@ -8,7 +8,7 @@ import { TransactionItem } from './components/transaction-item/transaction-item.
 import { NoTransaction } from './components/no-transaction/no-transaction.component';
 import { TransactionsService } from '@shared/transactions/services/transactions.service';
 import { SearchComponent } from './components/search/search/search.component';
-import { firstValueFrom } from 'rxjs';
+import { rxResource } from '@angular/core/rxjs-interop';
 
 @Component({
   selector: 'app-list',
@@ -25,10 +25,10 @@ export class List {
 
   search = signal('');
 
-  resourceRef = resource({
+  resourceRef = rxResource({
     params: () => this.search(),
-    loader: () => {
-      return firstValueFrom(this.transactionsService.getAll(this.search()));
+    stream: () => {
+      return this.transactionsService.getAll(this.search());
     },
     defaultValue: [],
   });
