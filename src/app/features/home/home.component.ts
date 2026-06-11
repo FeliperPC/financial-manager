fc87de0cc03c182023439371e193c5aed2f24e07
Felipe Padovani Caetano <padovani1010@gmail.com>
2026-06-11 18:31:09 -0300

    feat: enhance home component with loading states and chart interaction
    - Updated home.component.html to include a loading state and error handling for the pie chart.
    - Introduced a chart-container for better layout and user interaction with buttons to control chart visibility.
    - Added styles in home.component.scss for card and chart-container elements.
    - Modified home.component.ts to include a signal for managing chart visibility.

--- src/app/features/home/home.component.ts
+++ src/app/features/home/home.component.ts
@@ -1,13 +1,26 @@
-import { Component, computed, input } from '@angular/core';
+import { Component, computed, input, signal } from '@angular/core';
 import { Transaction } from '@shared/transactions/interfaces/transaction';
 import { Balance } from './components/balance/balance.component';
 import { PieChartComponent } from './components/pie-chart/pie-chart.component';
 import { TransactionType } from '@shared/transactions/enums/transaction-type';
 import { sumTransactionsByType } from '@shared/transactions/functions/sumTransactionsByType';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { CustomColorDirective } from '@shared/material/buttons/directives/custom-color.directive';
+import { MatProgressBarModule } from '@angular/material/progress-bar';
 
 @Component({
   selector: 'app-list',
-  imports: [Balance, PieChartComponent],
+  imports: [
+    Balance,
+    PieChartComponent,
+    MatIconModule,
+    MatButtonModule,
+    MatCardModule,
+    CustomColorDirective,
+    MatProgressBarModule,
+  ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
 })
@@ -19,6 +32,8 @@ export class Home {
   totalOutcome = computed(() =>
     sumTransactionsByType(this.transactions(), TransactionType.OUTCOME),
   );
+
+  canShowChart = signal(false);
 
   transactionPieConfig = computed(() => {
     return {
