88f2822eec28949af16ae4b34c26022c1e3c5a91
Felipe Padovani Caetano <padovani1010@gmail.com>
2026-06-11 11:30:59 -0300

    feat: add PieChartComponent and integrate Chart.js for pie chart visualization
    - Introduced PieChartComponent to render pie charts using Chart.js.
    - Created PieChartConfig interface to define the configuration structure for the pie chart.
    - Updated package.json and package-lock.json to include Chart.js as a dependency.

---
+++ src/app/features/home/components/pie-chart/pie-chart.component.ts
@@ -0,0 +1,31 @@
+import { Component, effect, ElementRef, input, viewChild } from '@angular/core';
+import Chart from 'chart.js/auto';
+import { PieChartConfig } from './pie-chart.interface';
+
+@Component({
+  selector: 'app-pie-chart',
+  imports: [],
+  template: `<canvas #canvas></canvas>`,
+})
+export class PieChartComponent {
+  canvas = viewChild.required<ElementRef>('canvas');
+
+  config = input.required<PieChartConfig>();
+
+  constructor() {
+    effect(() => {
+      new Chart(this.canvas().nativeElement, {
+        type: 'pie',
+        data: {
+          labels: this.config().labels,
+          datasets: [
+            {
+              label: this.config().dataLabel,
+              data: this.config().data,
+            },
+          ],
+        },
+      });
+    });
+  }
+}
