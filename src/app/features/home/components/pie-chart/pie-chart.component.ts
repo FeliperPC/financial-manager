39328da96776f2957d5820ed65276520f8cfca09
Felipe Padovani Caetano <padovani1010@gmail.com>
2026-06-11 11:57:42 -0300

    refact: improve PieChartComponent lifecycle management and chart instance handling
    - Introduced DestroyRef for proper cleanup of chart instances on component destruction.
    - Refactored chart creation logic into a separate method for better readability and maintainability.
    - Ensured the chart instance is destroyed before creating a new one to prevent memory leaks.

--- src/app/features/home/components/pie-chart/pie-chart.component.ts
+++ src/app/features/home/components/pie-chart/pie-chart.component.ts
@@ -1,4 +1,4 @@
-import { Component, effect, ElementRef, input, viewChild } from '@angular/core';
+import { Component, DestroyRef, effect, ElementRef, inject, input, viewChild } from '@angular/core';
 import Chart from 'chart.js/auto';
 import { PieChartConfig } from './pie-chart.interface';
 
@@ -11,21 +11,35 @@ export class PieChartComponent {
   canvas = viewChild.required<ElementRef>('canvas');
 
   config = input.required<PieChartConfig>();
+  private destroyRef = inject(DestroyRef);
+
+  private chart: Chart | null = null;
 
   constructor() {
     effect(() => {
-      new Chart(this.canvas().nativeElement, {
-        type: 'pie',
-        data: {
-          labels: this.config().labels,
-          datasets: [
-            {
-              label: this.config().dataLabel,
-              data: this.config().data,
-            },
-          ],
-        },
-      });
+      this.destroyChartInstance();
+      this.chart = this.createChartInstance();
+    });
+
+    this.destroyRef.onDestroy(() => this.destroyChartInstance());
+  }
+
+  private destroyChartInstance() {
+    this.chart?.destroy();
+  }
+
+  private createChartInstance() {
+    return new Chart(this.canvas().nativeElement, {
+      type: 'pie',
+      data: {
+        labels: this.config().labels,
+        datasets: [
+          {
+            label: this.config().dataLabel,
+            data: this.config().data,
+          },
+        ],
+      },
     });
   }
 }
